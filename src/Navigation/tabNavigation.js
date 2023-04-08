145f32993360ebce8dc8d45256eca30c406cbe87
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-08 13:43:35 +0100

    Prettier and eslint encorporation
    Updated files to abide by Linter rules

--- src/Navigation/tabNavigation.js
+++ src/Navigation/tabNavigation.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable react/no-unstable-nested-components */
 import { View, Text } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -6,64 +6,77 @@ import { Entypo, Ionicons } from '@expo/vector-icons';
 
 import ChatScreen from '../Screens/chatScreen';
 
-const TEMP_COMPONENT = () => {
+function TEMP_COMPONENT() {
   return (
     <View>
-      <Text> IT'S GONNA SHOW LADS YEESSSS </Text>
+      <Text> ITS GONNA SHOW LADS YEESSSS </Text>
     </View>
   );
 }
 
 const Tab = createBottomTabNavigator();
 
-const HomeTabNav = () => {
+function HomeTabNav() {
   return (
-    <Tab.Navigator initialRouteName='Chats'
+    <Tab.Navigator
+      initialRouteName="Chats"
       screenOptions={{
         tabBarStyle: { backgroundColor: 'whitesmoke' },
-        headerStyle: { backgroundColor: 'whitesmoke' }
+        headerStyle: { backgroundColor: 'whitesmoke' },
       }}
     >
-      <Tab.Screen name='Status' component={TEMP_COMPONENT}
+      <Tab.Screen
+        name="Status"
+        component={TEMP_COMPONENT}
         options={{
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name='logo-whatsapp' size={size} color={color} />
-          )
+            <Ionicons name="logo-whatsapp" size={size} color={color} />
+          ),
         }}
       />
-      <Tab.Screen name='Calls' component={TEMP_COMPONENT}
+      <Tab.Screen
+        name="Calls"
+        component={TEMP_COMPONENT}
         options={{
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name='call-outline' size={size} color={color} />
-          )
+            <Ionicons name="call-outline" size={size} color={color} />
+          ),
         }}
       />
-      <Tab.Screen name='Camera' component={TEMP_COMPONENT}
+      <Tab.Screen
+        name="Camera"
+        component={TEMP_COMPONENT}
         options={{
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name='camera-outline' size={size} color={color} />
-          )
+            <Ionicons name="camera-outline" size={size} color={color} />
+          ),
         }}
       />
-      <Tab.Screen name='Chats' component={ChatScreen}
-        options={({ navigation }) => (
-          {
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name='newspaper-outline' size={size} color={color} />
-            ),
-            headerRight: () => (
-              <Entypo
-                onPress={() => navigation.navigate('Contacts')}
-                name='new-message' size={18} color={'royalblue'}
-                style={{ marginRight: 15 }} />
-            )
-          })}
+      <Tab.Screen
+        name="Chats"
+        component={ChatScreen}
+        options={({ navigation }) => ({
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="newspaper-outline" size={size} color={color} />
+          ),
+          headerRight: () => (
+            <Entypo
+              onPress={() => navigation.navigate('Contacts')}
+              name="new-message"
+              size={18}
+              color="royalblue"
+              style={{ marginRight: 15 }}
+            />
+          ),
+        })}
       />
-      <Tab.Screen name='Settings' component={TEMP_COMPONENT}
+      <Tab.Screen
+        name="Settings"
+        component={TEMP_COMPONENT}
         options={{
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name='settings-outline' size={size} color={color} />
-          )
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
         }}
       />
     </Tab.Navigator>
