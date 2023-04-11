9b36ec1518149f29f3e2eb11e7dc5f3f0f213279
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-11 17:53:58 +0100

    Create apiTest.js

---
+++ src/api/apiValidation/apiTest.js
@@ -0,0 +1,73 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-param-reassign */
+import { useState } from 'react';
+import axios from 'axios';
+import { Button, View, Text } from 'react-native';
+
+import userList from '../../data/users.json';
+
+const authorisationToken = 'access';
+const baseUrl = 'http://localhost:3333/api/1.0.0';
+
+const api = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Authorization': authorisationToken,
+  },
+});
+
+export default function ApiTest() {
+  const [err, setErr] = useState(null);
+
+  api.interceptors.request.use(
+    (config) => {
+      config.headers['X-Authorization'] = `${authorisationToken}`;
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+
+  const first_name = 'ags';
+  const last_name = 'asdf';
+  const email = 'ags.f@test.com';
+  const password = 'Password@123';
+
+  const addUsers = async () => {
+    try {
+      const response = await api.post('/user', {
+        first_name,
+        last_name,
+        email,
+        password,
+      });
+
+      // Handle successful response here
+      console.log(response.data);
+    } catch (error) {
+      // Handle error response here
+      console.error(error);
+      setErr('Failed to add users');
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const response = await api.get('/user/4');
+      // Handle successful response here
+      console.log(response.data);
+    } catch (error) {
+      // Handle error response here
+      console.error(error);
+      setErr('Failed to get user');
+    }
+  };
+
+  return (
+    <View>
+      {/* <Button onPress={addUsers} title="Add Users" /> */}
+      <Button onPress={getUsers} title="Get Users" />
+      {err && <Text>{err}</Text>}
+    </View>
+  );
+}
