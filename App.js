60f372ec3f9d289e40fa0ba63d488f27ea1f9da4
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-11 14:09:21 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
 /* eslint-disable react/style-prop-object */
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import axios from 'axios';
 
 import { AppStack, LoginStack } from './src/Navigation/stackNavigation';
-
-const api = axios.create({
-  baseURL: 'http://localhost:3333/api/1.0.0',
-});
-
-const createNewUser = {
-  firstName: 'abbie',
-  lastName: 'lang',
-  email: 'abs.lang@email.com',
-  password: 'password',
-};
 
 const styles = StyleSheet.create({
   container: {
@@ -26,7 +14,6 @@ const styles = StyleSheet.create({
 
 export default function App() {
   return (
-    // <SafeAreaView style={styles.container}>
     <SafeAreaView style={styles.container}>
       <LoginStack />
       {/* <AppStack /> */}
