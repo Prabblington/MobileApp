616484c99f4c61a8cd6bd037b1b09b04c815b37f
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-13 19:12:52 +0100

    Update apiTestComponent.js
    Added await to prevent functions completing before another

--- src/api/apiTestComponent.js
+++ src/api/apiTestComponent.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, Button, Text } from 'react-native';
-import { loginTest, signupTest, getUserTest } from './getOpenAiJson';
+import { loginTest, signupTest, getUserTest } from './apiFunctionsTest';
 
 function apiTestComponent() {
   const [userData, setUserData] = useState(null);
@@ -10,8 +10,8 @@ function apiTestComponent() {
     try {
       // const data = await getUser(2); // call the getUser function with a specific user ID
       // setUserData(data);
-      loginTest();
-      getUserTest(6);
+      await loginTest();
+      await getUserTest(6);
       setError(null);
     } catch (err) {
       setError(err.message);
