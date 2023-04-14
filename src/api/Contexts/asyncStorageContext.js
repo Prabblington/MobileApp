e424b5cadc88084beb0a1d6671412a50df69b485
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-14 19:47:02 +0100

    Update asyncStorageContext.js

--- src/api/Contexts/asyncStorageContext.js
+++ src/api/Contexts/asyncStorageContext.js
@@ -5,37 +5,18 @@ const UseAsyncStorage = async () => {
   const [data, setData] = useState(null);
 
   const setItem = async (key, value) => {
-    try {
-      await AsyncStorage.setItem(key, value);
-
-      setData(value);
-    } catch (err) {
-      console.warn(`Failed to set ${key} to ${value}`);
-      throw new Error(err);
-    }
+    await AsyncStorage.setItem(key, value);
+    setData(value);
   };
 
   const getItem = async (key) => {
-    try {
-      const value = await AsyncStorage.getItem(key);
-      if (value !== null) {
-        setData(value);
-        return value;
-      }
-    } catch (err) {
-      console.warn(`Failed to get ${key}`);
-      throw new Error(err);
-    }
-    return null;
+    const value = await AsyncStorage.getItem(key);
+    setData(value);
   };
 
   const removeItem = async (key) => {
-    try {
-      await localStorage.removeItem(key);
-      setData(null);
-    } catch (err) {
-      throw new Error(err);
-    }
+    await localStorage.removeItem(key);
+    setData(null);
   };
 
   return { data, setItem, getItem, removeItem };
