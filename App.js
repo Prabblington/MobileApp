e3cd4b4e73a3b1506f76877a89fb7f83cff7e962
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-17 23:50:57 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 /* eslint-disable react/style-prop-object */
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { createContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { AppStack, LoginStack } from './src/Navigation/stackNavigation';
-import ApiTestComponent from './src/api/Client/apiTestComponent';
+import { AuthContext } from './src/Navigation/Context/authManager';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,20 +16,33 @@ const styles = StyleSheet.create({
   },
 });
 
-// const authContext = createContext({
-//   authState: {
-//     id: '',
-//     email: '',
-//     password: '',
-//     loggedIn: false,
-//   },
-//   setAuthState: () => {},
-// });
-
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // const [isLoading, setIsLoading] = useState(true); // added loading state
+
+  // useEffect(() => {
+  //   const checkLoggedIn = async () => {
+  //     const loggedInStatus = await AsyncStorage.getItem('isLoggedIn');
+
+  //     setIsLoading(false);
+  //   };
+  //   checkLoggedIn();
+  // }, []);
+
+  // if (isLoading === true) { // render loading screen while checking logged in status
+  //   return (
+  //     <SafeAreaView style={styles.container}>
+  //       <LoadingScreen />
+  //       <StatusBar style="auto" />
+  //     </SafeAreaView>
+  //   );
+  // }
+
   return (
     <SafeAreaView style={styles.container}>
-      <LoginStack />
+      {isLoggedIn ? <AppStack /> : <LoginStack />}
+
       {/* <AppStack /> */}
       {/* <ApiTestComponent /> */}
 
