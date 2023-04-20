1dea53dd5ace61cab8d978eccf059a038ae64086
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-20 15:22:19 +0100

    Update searchScreen.js

--- src/Screens/searchScreen.js
+++ src/Screens/searchScreen.js
@@ -4,16 +4,18 @@ import { KeyboardAvoidingView } from 'react-native-web';
 import { useState } from 'react';
 
 import CustInput from '../Components/Input/custInput';
+import CustButton from '../Components/Input/custButton';
 import ChatListRenderer from '../Components/Chats/chatListRender';
 import searchUser from '../api/Client/User/searchUser';
 
 export default function SearchScreen() {
   const [query, setQuery] = useState('');
+  const [contactsOrAll, setContactsOrAll] = useState('all');
   const [err, setErr] = useState(null);
 
   const result = async () => {
     try {
-      const trySearch = await searchUser(query);
+      const trySearch = await searchUser(query, contactsOrAll);
       setErr(null);
     } catch (e) {
       setErr(e);
@@ -28,6 +30,12 @@ export default function SearchScreen() {
         placeholder="e.g. John Smith"
         value={query}
         setValue={setQuery}
+      />
+      <CustButton
+        onPress={result}
+        title="Search Button"
+        accessibilityLabel="press button to search for all users containing name"
+        buttonText="Search"
       />
       <View>
         <FlatList
