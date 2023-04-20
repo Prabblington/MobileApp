f7a14bb847590715e1f562869b0e869d2545f2b5
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-20 15:15:39 +0100

    Update searchUser.js

--- src/api/Client/User/searchUser.js
+++ src/api/Client/User/searchUser.js
@@ -1,20 +1,29 @@
 import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { config } from '../../AxiosConfig/axiosConfig';
 
 // GET /search
-export default function searchUser(query) {
-  return axios
-    .get('./search', query, config)
-    .then(
-      (response) =>
-        // DO SOMETHING WITH THE RESPONSE
-        true
-    )
-    .catch(
-      async (error) =>
-        // DO SOMETHING WHEN AN ERROR IS THROWN
-        false
-    );
+export default function searchUser(userQuery, searchInParam) {
+  const queryParams = {
+    q: `${userQuery}`,
+    filter: 'first_name, last_name, email',
+    limit: 20,
+    search_in: `${searchInParam}`,
+    offset: 0,
+  };
+
+  let result = {};
+
+  axios
+    .get('./search', { queryParams }, config)
+    .then((response) => {
+      result = response.data;
+      // DO SOMETHING WITH THE RESPONSE
+    })
+    .catch(async (error) => {
+      // DO SOMETHING WHEN AN ERROR IS THROWN
+      console.error(error);
+    });
+
+  return result;
 }
