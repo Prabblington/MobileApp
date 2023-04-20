f37c1728cdba9cb9286b574a9c7d7e58e34dd44e
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-20 14:29:29 +0100

    Update searchScreen.js

--- src/Screens/searchScreen.js
+++ src/Screens/searchScreen.js
@@ -1,21 +1,36 @@
 import { FlatList } from 'react-native';
 import { KeyboardAvoidingView } from 'react-native-web';
 
+import { useState } from 'react';
+import CustInput from '../Components/Input/custInput';
 import ChatListRenderer from '../Components/Chats/chatListRender';
-import chat from '../data/chats.json';
+import searchUser from '../api/Client/User/searchUser';
 
 export default function ChatScreen() {
+  const [query, setQuery] = useState('');
+  const [err, setErr] = useState(null);
+
+  const result = async () => {
+    try {
+      const trySearch = await searchUser(query);
+      setErr(null);
+    } catch (e) {
+      setErr(e);
+      console.warn(e);
+    }
+  };
+
   return (
     <KeyboardAvoidingView>
       <CustInput
-        titleText="Password"
-        placeholder="e.g. password@123"
-        value={password}
-        setValue={setPassword}
+        titleText="Search..."
+        placeholder="e.g. John Smith"
+        value={query}
+        setValue={setQuery}
         secureTextEntry
       />
       <FlatList
-        data={chat}
+        data={result}
         renderItem={({ item }) => <ChatListRenderer chat={item} />}
         style={{ backgroundColor: 'whitesmoke' }}
       />
