a13da9122702f8d09c28ebfd06e63dfcb04d5621
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-22 16:22:17 +0100

    Update drawerNavigation.js

--- src/Navigation/drawerNavigation.js
+++ src/Navigation/drawerNavigation.js
@@ -1,2 +1,13 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+function AppSettings() {
+  <Drawer.Navigator screenOptions={{ headerShown: false }}>
+    <Drawer.Screen name="Contacts Settings" component={contactSettings} />
+    <Drawer.Screen name="General Settings" component={generalSettings} />
+    <Drawer.Screen name="Group Settings" component={groupSettings} />
+  </Drawer.Navigator>;
+}
+
+export default AppSettings;
