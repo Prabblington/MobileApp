67b470b1c6fafa45cfb1b60c86880fdffa7f3662
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-22 16:00:00 +0100

    Update mainNavigation.js

--- src/Navigation/mainNavigation.js
+++ src/Navigation/mainNavigation.js
@@ -6,10 +6,6 @@ import { AppStack, LoginStack } from './stackNavigation';
 export default function MainNavigation() {
   const { isLoggedIn } = useContext(AuthContext);
 
-  // useEffect(() => {
-  //   console.log(`${isLoggedIn}, token: ${token}, user: ${user}`);
-  // }, [isLoggedIn, token, user]);
-
   // eslint-disable-next-line react/jsx-no-useless-fragment
   return <>{isLoggedIn ? <AppStack /> : <LoginStack />}</>;
 }
