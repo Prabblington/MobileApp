c82da09fbd10780bffc0a8dce159885dac717718
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-22 17:34:44 +0100

    Update Blocked Contact Options
    Added getBlockedContacts
    Finished blockContact and unblockContact

---
+++ src/api/Client/Contact Management/blockContactOptions.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+function getBlockedContacts(cfg) {
+  return axios
+    .post(`/blocked`, cfg)
+    .then(async (response) => {
+      console.log(`Users blocked: ${response.data}`);
+      console.log(response.status);
+    })
+    .catch(async (error) => {
+      console.log(error);
+    });
+}
+
+function blockContact(userID, cfg) {
+  return axios
+    .post(`/user/${userID}/block`, cfg)
+    .then(async (response) => {
+      console.log('User blocked!');
+      console.log(response.status);
+    })
+    .catch(async (error) => {
+      console.log(error);
+    });
+}
+
+function unblockContact(userID, cfg) {
+  return axios
+    .delete(`/user/${userID}/block`, cfg)
+    .then(async (response) => {
+      console.log('User unblocked!');
+      console.log(response.status);
+    })
+    .catch(async (error) => {
+      console.log(error);
+    });
+}
+
+export { getBlockedContacts, blockContact, unblockContact };
--- src/api/Client/Contact Management/blockUserOptions.js
+++
@@ -1,9 +0,0 @@
-function blockUser() {
-  console.log('User Blocked!');
-}
-
-function unblockUser() {
-  console.log('User Unblocked!');
-}
-
-export { blockUser, unblockUser };
