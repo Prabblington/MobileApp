42abf4f10ed954f92533accb9b43ee2deb2ecdc7
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-23 15:22:59 +0100

    Update profilePage.js
    Update design
    NOTE: local image paths don't work in this component, all fixes tried. Will experiment with URI later once api is capable of uploading and pulling images from the database

--- src/Components/Settings/profilePage.js
+++ src/Components/Settings/profilePage.js
@@ -1,28 +1,83 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
+
+import contactpfp from '../../images/logo2.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    alignItems: 'center',
   },
-  profileContainer: {},
+  profileContainer: {
+    marginTop: '30%',
+    justifyContent: 'centre',
+  },
   userImage: {
-    margin: 15,
-    width: 60,
-    height: 60,
+    alignSelf: 'center',
+    backgroundColor: 'red',
+    width: 120,
+    height: 120,
+    aspectRatio: 1,
+    borderRadius: 20,
   },
-  nameBox: {},
-  status: {},
-  userOptions: {},
+  nameBox: {
+    flex: 1,
+    marginVertical: 8,
+  },
+  status: {
+    marginVertical: 1,
+  },
+  header: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  subHeader: {
+    color: 'grey',
+    maxWidth: '90%',
+    textAlign: 'center',
+  },
+  userOptionsContainer: {
+    flex: 1,
+
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'lightgray',
+  },
+  userOptions: {
+    flexDirection: 'row',
+    marginTop: 10,
+    marginBottom: 5,
+    marginHorizontal: 8,
+    padding: 4,
+
+    borderWidth: 10,
+    borderColor: 'red',
+    borderRadius: 12,
+  },
 });
 
-export default function ProfilePage(user) {
+export default function ProfilePage({ user }) {
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
-        <Text> Image </Text>
-        <Text> Name </Text>
-        <Text> Status </Text>
+        <View style={styles.userImage}>
+          <Image
+            style={styles.image}
+            // source={user.profile.image}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.nameBox}>
+          <Text style={styles.header}> John Smith </Text>
+        </View>
+        <View style={styles.status}>
+          <Text style={styles.subHeader}> Online: At work atm! </Text>
+        </View>
+        <View style={styles.userOptionsContainer}>
+          <View style={styles.userOptions}>
+            <Text> Icon 1 </Text>
+            <Text> Icon 1 </Text>
+            <Text> Icon 1 </Text>
+          </View>
+        </View>
       </View>
     </View>
   );
