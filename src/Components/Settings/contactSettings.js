4008ac2a9eeab68055f42fe38f418efab8156164
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-23 19:39:28 +0100

    Create contactSettings.js

---
+++ src/Components/Settings/contactSettings.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+
+const settings = {
+  viewContact: false,
+  blocked: false,
+};
+
+export default function ContactSettings() {
+  return (
+    <View>
+      <Text> Contact Settings drawer </Text>
+    </View>
+  );
+}
