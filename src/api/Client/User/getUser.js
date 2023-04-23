443fe4c2ef4571103f8d115f62de3494e7ee507d
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-23 15:21:57 +0100

    Update getUser.js

--- src/api/Client/User/getUser.js
+++ src/api/Client/User/getUser.js
@@ -1,21 +1,37 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const getUserTest = async (cfg) => {
-  const user = await AsyncStorage.getItem('userData');
-  const parseUser = user !== null ? JSON.parse(user) : null;
-  const userID = parseUser.id;
+const returnCurrentUser = async (cfg) => {
+  try {
+    const user = await AsyncStorage.getItem('userData');
+    const parseUser = user !== null ? await JSON.parse(user) : null;
 
-  axios
-    .get(`/user/${userID}`, cfg)
-    .then((response) => {
-      const firstName = response.data.first_name;
-      const lastName = response.data.last_name;
-      const { email } = response.data;
-      console.log(`User ${user} data: ${firstName} ${lastName} ${email}`);
-    })
-    .catch((error) => {
-      console.log(error);
-      throw new Error(error);
-    });
+    return user;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+const getUser = async (cfg) => {
+  try {
+    const user = await AsyncStorage.getItem('userData');
+    const parseUser = user !== null ? await JSON.parse(user) : null;
+    const userID = parseUser.id;
+
+    axios
+      .get(`/user/${userID}`, cfg)
+      .then((response) => {
+        const firstName = response.data.first_name;
+        const lastName = response.data.last_name;
+        const { email } = response.data;
+        console.log(`User ${user} data: ${firstName} ${lastName} ${email}`);
+      })
+      .catch((error) => {
+        console.log(error);
+        throw new Error(error);
+      });
+  } catch (e) {
+    console.log(e);
+  }
 };
