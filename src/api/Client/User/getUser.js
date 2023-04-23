936f1b5314ea2318bcb72850016435817863d136
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-23 15:42:15 +0100

    Update getUser.js

--- src/api/Client/User/getUser.js
+++ src/api/Client/User/getUser.js
@@ -1,38 +1,34 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const returnCurrentUser = async (cfg) => {
+const returnCurrentUser = async () => {
   try {
     const user = await AsyncStorage.getItem('userData');
     const parseUser = user !== null ? await JSON.parse(user) : null;
-
-    return user;
+    return parseUser;
   } catch (e) {
-    console.log(e);
-    return null;
+    console.warn(e);
+    return undefined;
   }
 };
 
-const getUser = async (cfg) => {
+const getUser = async (userID, cfg) => {
   try {
-    const user = await AsyncStorage.getItem('userData');
-    const parseUser = user !== null ? await JSON.parse(user) : null;
-    const userID = parseUser.id;
-
     axios
       .get(`/user/${userID}`, cfg)
       .then((response) => {
         const firstName = response.data.first_name;
         const lastName = response.data.last_name;
         const { email } = response.data;
-        console.log(`User ${user} data: ${firstName} ${lastName} ${email}`);
+        console.log(`User ${userID} data: ${firstName} ${lastName} ${email}`);
+        return { firstName, lastName, email };
       })
       .catch((error) => {
-        console.log(error);
-        throw new Error(error);
+        console.warn(error);
+        return null;
       });
   } catch (e) {
-    console.log(e);
+    console.warn(e);
   }
 };
 
