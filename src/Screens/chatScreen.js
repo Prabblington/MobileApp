e687fbf93d878480e1982b3d1d2e9d5b8f320049
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-24 17:52:46 +0100

    Update Chat api

--- src/Screens/chatScreen.js
+++ src/Screens/chatScreen.js
@@ -1,33 +1,28 @@
 import { FlatList } from 'react-native';
-// import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import ChatListRenderer from '../Components/Chats/chatListRender';
-import chat from '../data/chats.json';
-// import getChatList from '../api/Client/Chat Management/getChatList';
-// import { AuthContext } from '../Navigation/Context/authManager';
+// import chat from '../data/chats.json';
+import getChatList from '../api/Client/Chat Management/getChatList';
+import { AuthContext } from '../Navigation/Context/authManager';
 
 export default function ChatScreen() {
-  // const { axiosConfig } = useContext(AuthContext);
-  // const [chatList, setChatList] = useState({});
+  const { axiosConfig } = useContext(AuthContext);
+  const [chatList, setChatList] = useState({});
 
-  // const getChatsTest = async () => {
-  //   await getChatList(axiosConfig);
-  // };
-  // console.log(getChatList);
-
-  // useEffect(() => {
-  //   async function getChats() {
-  //     const result = await getChatList(axiosConfig);
-  //     console.log(result.data);
-  //     setChatList(result.data);
-  //     console.log(JSON.stringify(result));
-  //   }
-  //   getChats();
-  // }, [chatList]);
+  useEffect(() => {
+    async function getChats() {
+      const result = await getChatList(axiosConfig);
+      console.log(result);
+      setChatList(result);
+      console.log(JSON.stringify(result));
+    }
+    getChats();
+  }, []);
 
   return (
     <FlatList
-      data={chat}
+      data={chatList}
       renderItem={({ item }) => <ChatListRenderer chat={item} />}
       style={{ backgroundColor: 'whitesmoke' }}
     />
--- src/api/Client/Chat Management/getChatList.js
+++ src/api/Client/Chat Management/getChatList.js
@@ -2,12 +2,11 @@ import axios from 'axios';
 
 export default async function getChatList(cfg) {
   try {
-    const chats = await axios.get('./chat', cfg).then((response) => {
+    return axios.get('./chat', cfg).then((response) => {
       console.log(`response.data: ${response.data}`);
       console.log(response.status);
       return response.data;
     });
-    return chats;
   } catch (e) {
     console.log(e);
     return null;
