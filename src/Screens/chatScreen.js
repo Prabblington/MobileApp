c9c521af9b6df4699e6a9fc0753346af6dc0782a
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-24 16:44:12 +0100

    Update chatScreen.js

--- src/Screens/chatScreen.js
+++ src/Screens/chatScreen.js
@@ -1,9 +1,30 @@
 import { FlatList } from 'react-native';
+// import { useContext, useEffect, useState } from 'react';
 
 import ChatListRenderer from '../Components/Chats/chatListRender';
 import chat from '../data/chats.json';
+// import getChatList from '../api/Client/Chat Management/getChatList';
+// import { AuthContext } from '../Navigation/Context/authManager';
 
 export default function ChatScreen() {
+  // const { axiosConfig } = useContext(AuthContext);
+  // const [chatList, setChatList] = useState({});
+
+  // const getChatsTest = async () => {
+  //   await getChatList(axiosConfig);
+  // };
+  // console.log(getChatList);
+
+  // useEffect(() => {
+  //   async function getChats() {
+  //     const result = await getChatList(axiosConfig);
+  //     console.log(result.data);
+  //     setChatList(result.data);
+  //     console.log(JSON.stringify(result));
+  //   }
+  //   getChats();
+  // }, [chatList]);
+
   return (
     <FlatList
       data={chat}
