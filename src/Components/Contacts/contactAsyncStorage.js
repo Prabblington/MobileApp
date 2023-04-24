b8793c305c847d5ff02a39c827b1c8b9e8a3d7e4
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-24 13:05:22 +0100

    Create contactAsyncStorage.js

---
+++ src/Components/Contacts/contactAsyncStorage.js
@@ -0,0 +1,36 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+async function storeContactAsyncStorage({ contact }) {
+  try {
+    console.log('Contact Stored');
+    console.log(`store ${contact}`);
+    await AsyncStorage.setItem('viewContactDetails', JSON.stringify(contact));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getContactAsyncStorage() {
+  try {
+    const contactObject = await AsyncStorage.getItem('viewContactDetails');
+    return JSON.parse(contactObject);
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+async function deleteContactAsyncStorage() {
+  try {
+    console.log('Contact Deleted');
+    await AsyncStorage.removeItem('viewContactDetails');
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export {
+  storeContactAsyncStorage,
+  deleteContactAsyncStorage,
+  getContactAsyncStorage,
+};
