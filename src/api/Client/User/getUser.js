c928d58535baaf172e64e634432d38d8999c32cb
A. L <78857516+Prabblington@users.noreply.github.com>
2023-04-25 16:23:40 +0100

    Update getUser.js

--- src/api/Client/User/getUser.js
+++ src/api/Client/User/getUser.js
@@ -5,10 +5,8 @@ async function returnCurrentUserID() {
   try {
     const user = await AsyncStorage.getItem('userData');
     const parsedUser = user !== null ? await JSON.parse(user) : null;
-    console.log(parsedUser.id);
     return parsedUser.id;
   } catch (e) {
-    console.log('ReturnCurrentUserID ERROR');
     console.warn(e);
     return null;
   }
@@ -16,7 +14,16 @@ async function returnCurrentUserID() {
 
 async function getUser(userID, cfg) {
   try {
-    return axios.get(`/user/${userID}`, cfg).then((response) => response.data);
+    const result = await axios.get(`/user/${userID}`, cfg).then((response) => {
+      if (result.message === 'Network Error') {
+        return returnCurrentUserID();
+      }
+      if (result.status === 200) {
+        return result.data;
+      }
+    });
+
+    return result;
   } catch (e) {
     console.warn(e);
     return null;
